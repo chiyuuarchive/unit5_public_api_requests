9cf0cde9d480abd9251daa0fbbfb7cbfbe964a0e
chiyuuarchive <85938105+chiyuuarchive@users.noreply.github.com>
2021-07-23 09:12:16 +0200

    Update script.js
    Tidying up the file and updated the format of the api

--- js/script.js
+++ js/script.js
@@ -22,7 +22,9 @@ const searchDiv = document.querySelector(".search-container");
 const galleryDiv = document.querySelector(".gallery");
 const script = document.querySelector("script");
 
-
+/* =======================
+Response handling
+======================== */
 /**
  * [Fetches data from an API and convert the body text to JSON. Also checks for potential errors from API server]
  *
@@ -33,10 +35,12 @@ function getEmployeeData (url) {
     return fetch(url)
         .then (checkStatus)
         .then (response => response.json())
-        .catch (error => console.log("Error:", error))
-        
+        .catch (error => console.log("Error:", error))   
 }
 
+/* =======================
+Error handling
+======================== */
 /**
  * [Captures the response and check if status is OK (returns a promise if resolved and optionally an Error object if rejected)]
  *
@@ -51,34 +55,26 @@ function checkStatus (response) {
     }
 }
 
-/**
- * [Calls the "addCard" function to generate employee cards from the parsed JSON data]
- *
- * @param {string} url: An uniform resource locator to an API.
- */
-function setGalleryUp (api) {
-    getEmployeeData(api)
-    .then(data => {        
-        for (let i = 0; i < data.results.length; i++) {
-            addCard(data.results, i);
-            console.log(data.results[i]);
-        }
-    })
-}
-
+/* =======================
+Search Markup
+======================== */
 /**
  * [Appends a search bar to the header element]
  */
-function addSearchBar () {
-const searchBarDiv = document.querySelector("div.search-container");
-let html = `<form action="#" method="get">
-                <input type="search" id="search-input" class="search-input" placeholder="Search...">
-                <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
-            </form>`;
-
-searchBarDiv.innerHTML = html;
+ function addSearchBar () {
+    const searchBarDiv = document.querySelector("div.search-container");
+    let html = `<form action="#" method="get">
+                    <input type="search" id="search-input" class="search-input" placeholder="Search...">
+                    <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+                </form>`;
+    
+    searchBarDiv.innerHTML = html;
 }
 
+
+/* =======================
+Gallery Markup
+======================== */
 /**
  * [Generate employee cards from the parsed JSON data with added search and modal window creation functionalities.]
  *
@@ -111,13 +107,11 @@ function addCard (data, index) {
         for (let i = 0; i < cardDivs.length; i++) {
             if (!cardDivs[i].querySelector("div.card-info-container").querySelector("#name").textContent.toLowerCase().includes(searchInput)) {
                 cardDivs[i].style.display = "none";
-
             }
             else {
                 cardDivs[i].style.display = "";
             }
         }
-
     })
 
     //Submit search feature
@@ -128,13 +122,11 @@ function addCard (data, index) {
         for (let i = 0; i < cardDivs.length; i++) {
             if (!cardDivs[i].querySelector("div.card-info-container").querySelector("#name").textContent.toLowerCase().includes(searchInput)) {
                 cardDivs[i].style.display = "none";
-
             }
             else {
                 cardDivs[i].style.display = "";
             }
         }
-
     })
 
     //Event handler - To bring up the modal window
@@ -157,6 +149,9 @@ function addCard (data, index) {
 }
 
 
+/* =======================
+Modal Markup
+======================== */
 /**
  * [Generate modal window with data and indices passed from "addCard" function. Added button navigation functionalities]
  *
@@ -167,7 +162,6 @@ function addCard (data, index) {
 function addModal (data, index) {
     const employeeInfo = data[index];
     const birthday = employeeInfo.dob.date;
-    
     const modalDiv = document.createElement("div");
     modalDiv.className = "modal-container";
     modalDiv.style.display = "";
@@ -191,8 +185,7 @@ function addModal (data, index) {
 
     modalDiv.innerHTML = html;
     script.insertAdjacentElement("beforebegin", modalDiv);
-
-
+    
     //Event handler - To remove the modal window.
     const button = document.querySelector("#modal-close-btn");
     button.addEventListener("click", () => {
@@ -261,13 +254,35 @@ function formatBirthday(data) {
     birthday = `${birthday.slice(5, 7)}/${birthday.slice(8, 10)}/${birthday.slice(0, 4)}`; 
 }
 
+
+/* =======================
+Setting the gallery up
+======================== */
+/**
+ * [Calls the "addCard" function to generate employee cards from the parsed JSON data]
+ *
+ * @param {string} url: An uniform resource locator to an API.
+ */
+ function setGalleryUp (api) {
+    getEmployeeData(api)
+    .then(data => {        
+        for (let i = 0; i < data.results.length; i++) {
+            addCard(data.results, i);
+            console.log(data.results[i]);
+        }
+    })
+}
+
 //Add search bar to the DOM
 addSearchBar();
 
 //Set up a directory with 12 employees
-setGalleryUp("https://randomuser.me/api/?results=12");
+setGalleryUp("https://randomuser.me/api/?results=12&nat=us");
 
-//Styling
+
+/* =======================
+Styling the DOM
+======================== */
 const headerPic = "https://picsum.photos/2500/400/";
 
 const backgroundImg = document.createElement("img");
